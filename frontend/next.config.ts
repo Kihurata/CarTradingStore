a61daa27bfa27deb849b6a039ec58fbfd00fbd72
Huy Châu <huygiachau4444@gmail.com>
2025-12-10 20:45:18 +0700

    update reduce weight

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -37,7 +37,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "i.ytimg.com" },
     ],
   },
-
+  output: "standalone",
   // ✅ Proxy /api/* → backend
   async rewrites() {
     const base = process.env.INTERNAL_API_BASE || "http://localhost:4000";
